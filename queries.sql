519c147f3da2a39eb15f6718879362810f525644
clementify <obiclementina92@gmail.com>
2023-09-04 09:23:32 +0100

    udated readme

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+
+SELECT age AS age_group, COUNT(*) FROM users GROUP BY age;
+
+-- SQL command to retrieve the users from the Users table along with their associated orders
+SELECT users.name, orders.order_id FROM users LEFT JOIN orders ON users.id  = orders.user_id;
+
+ UPDATE users SET age = 40;
+
+
+ 
+ DELETE FROM users;
